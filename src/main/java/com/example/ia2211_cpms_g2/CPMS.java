4097cbfa809aa3012e55a1a7f08552f84ab8452a
Sandun Viduranga <91661670+2019t00419@users.noreply.github.com>
2022-02-02 17:18:37 +0530

    ui

--- src/main/java/com/example/ia2211_cpms_g2/CPMS.java
+++ src/main/java/com/example/ia2211_cpms_g2/CPMS.java
@@ -11,7 +11,7 @@ public class CPMS extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CPMS.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 852, 480);
         stage.setTitle("UOC Car Park Management System");
         stage.setScene(scene);
         stage.show();
